e1fd23cffbcc3bb84637f1d212598f751c221eee
Douglas Oliveira <douglaspo_97@outlook.com>
2021-11-01 21:30:52 -0300

    flipping cards

--- src/App.js
+++ src/App.js
@@ -55,8 +55,7 @@ function App() {
         })
         resetTurn();
       } else {
-        
-        resetTurn();
+        setTimeout(() => resetTurn(), 1000);
       }
     }
   }, [choiceOne, choiceTwo])
@@ -75,7 +74,12 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          <SingleCard 
+            key={card.id} 
+            card={card} 
+            handleChoice={handleChoice} 
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
--- src/components/SingleCard/index.js
+++ src/components/SingleCard/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import BackCard from '../../img/cover.png';
 import './styles.css';
 
-export function SingleCard({ card, handleChoice }) {
+export function SingleCard({ card, handleChoice, flipped }) {
 
   function handleClick() {
     handleChoice(card)
@@ -12,7 +12,7 @@ export function SingleCard({ card, handleChoice }) {
   return (
     <div>
       <div className="card">
-        <div>
+        <div className={flipped ? "flipped" : ""}>
           <img className="front" src={card.src} alt="card Front" />
           <img className="back" src={BackCard} onClick={handleClick} alt="card back" />
         </div>
