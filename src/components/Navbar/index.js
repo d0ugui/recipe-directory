c5093cd9c1221dee6e7be5f81a090eaf464ac53d
Douglas Oliveira <douglaspo_97@outlook.com>
2021-11-25 22:08:15 -0300

    creating a custom context hook

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,12 @@
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
-import { ThemeContext } from '../../context/ThemeContext';
+import { useTheme } from '../../hooks/useTheme';
 import { SearchBar } from '../SearchBar';
 
 import './styles.css';
 
 export function Navbar() {
-  const { color } = useContext(ThemeContext);
+  const { color } = useTheme();
 
   return (
     <div className="navbar" style={{ background: color }}>
